0c909e879b9ec0f553f584c3a8ed26ee84200134
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-01-18 15:11:09 -0500

    added 40sp value to greetingTextView

--- ICE2/app/src/main/java/ca/georgiancollege/comp3025_w2024_week2/MainActivity.kt
+++ ICE2/app/src/main/java/ca/georgiancollege/comp3025_w2024_week2/MainActivity.kt
@@ -2,10 +2,19 @@ package ca.georgiancollege.comp3025_w2024_week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val greetTextView = findViewById<TextView>(R.id.greetingTextView)
+        greetTextView.text = "Hello Tom"
+
+        Log.i("onCreate", "Saying Hello")
     }
 }
