c43b74f1f21c151fd3db2b28aa125d147040ae30
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-01-25 13:36:17 -0500

    added project files for ICE3

---
+++ ICE3/app/src/main/java/ca/georgiancollege/comp3025_w2024_week3/MainActivity.kt
@@ -0,0 +1,13 @@
+package ca.georgiancollege.comp3025_w2024_week3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
