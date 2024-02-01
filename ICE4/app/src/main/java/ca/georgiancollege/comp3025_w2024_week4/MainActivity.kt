dbb0b9d8da2d5a60c5641e4db841de643a353a40
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-02-01 14:26:06 -0500

    added project files for ICE4

---
+++ ICE4/app/src/main/java/ca/georgiancollege/comp3025_w2024_week4/MainActivity.kt
@@ -0,0 +1,40 @@
+package ca.georgiancollege.comp3025_w2024_week4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import ca.georgiancollege.comp3025_w2024_week4.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // operator buttons
+        binding.clearButton.setOnClickListener{ view -> processOperatorButtons(view)}
+        binding.percentButton.setOnClickListener{view -> processOperatorButtons(view)}
+
+        // number buttons
+        binding.zeroButton.setOnClickListener{view -> processNumberButtons(view)}
+    }
+
+    private fun processOperatorButtons(view: View)
+    {
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        Log.i("operators", view.tag.toString())
+        resultTextView.text = view.tag.toString()
+    }
+
+    private fun processNumberButtons(view: View)
+    {
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        Log.i("operators", view.tag.toString())
+        resultTextView.text = view.tag.toString()
+    }
+}
