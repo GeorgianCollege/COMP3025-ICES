cec63e1aee0e3901eb5a25a597dda2a0fdde8678
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-02-01 16:05:42 -0500

    added simple decimal entry

--- ICE4/app/src/main/java/ca/georgiancollege/comp3025_w2024_week4/MainActivity.kt
+++ ICE4/app/src/main/java/ca/georgiancollege/comp3025_w2024_week4/MainActivity.kt
@@ -62,7 +62,17 @@ class MainActivity : AppCompatActivity()
 
     private fun processNumberButtons(view: View)
     {
-        resultLabelValue += view.tag.toString()
+        if(view.tag.toString() == ".")
+        {
+            if(!resultLabelValue.contains("."))
+            {
+                resultLabelValue += view.tag.toString()
+            }
+        }
+        else
+        {
+            resultLabelValue += view.tag.toString()
+        }
         binding.resultTextView.text = resultLabelValue
     }
 }
