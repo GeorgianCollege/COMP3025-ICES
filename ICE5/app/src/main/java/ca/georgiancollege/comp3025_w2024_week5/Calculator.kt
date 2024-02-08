96d586314791938f04d10a438ebd5c248da4f288
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-02-08 14:56:09 -0500

    transferred functions to Calculator class

---
+++ ICE5/app/src/main/java/ca/georgiancollege/comp3025_w2024_week5/Calculator.kt
@@ -0,0 +1,74 @@
+package ca.georgiancollege.comp3025_w2024_week5
+
+import android.view.View
+import ca.georgiancollege.comp3025_w2024_week5.databinding.ActivityMainBinding
+
+class Calculator(binding: ActivityMainBinding)
+{
+    private var m_resultLabelValue: String
+    private var m_binding: ActivityMainBinding
+
+    init
+    {
+        this.m_binding = binding
+        this.m_resultLabelValue = ""
+
+        initializeOnClickListeners()
+    }
+
+    private fun initializeOnClickListeners()
+    {
+        // operator buttons
+        this.m_binding.mulitplyButton.setOnClickListener { view -> processOperatorButtons(view) }
+        this.m_binding.divideButton.setOnClickListener { view -> processOperatorButtons(view) }
+        this.m_binding.addButton.setOnClickListener { view -> processOperatorButtons(view) }
+        this.m_binding.subtractButton.setOnClickListener { view -> processOperatorButtons(view) }
+
+        // extra buttons
+        this.m_binding.clearButton.setOnClickListener { view -> processExtraButtons(view) }
+        this.m_binding.percentButton.setOnClickListener { view -> processExtraButtons(view) }
+        this.m_binding.backspaceButton.setOnClickListener { view -> processExtraButtons(view) }
+        this.m_binding.plusMinusButton.setOnClickListener { view -> processExtraButtons(view) }
+
+        // number buttons
+        this.m_binding.zeroButton.setOnClickListener { view -> processNumberButtons(view) }
+        this.m_binding.oneButton.setOnClickListener { view -> processNumberButtons(view) }
+        this.m_binding.twoButton.setOnClickListener { view -> processNumberButtons(view) }
+        this.m_binding.threeButton.setOnClickListener { view -> processNumberButtons(view) }
+        this.m_binding.fourButton.setOnClickListener { view -> processNumberButtons(view) }
+        this.m_binding.fiveButton.setOnClickListener { view -> processNumberButtons(view) }
+        this.m_binding.sixButton.setOnClickListener { view -> processNumberButtons(view) }
+        this.m_binding.sevenButton.setOnClickListener { view -> processNumberButtons(view) }
+        this.m_binding.eightButton.setOnClickListener { view -> processNumberButtons(view) }
+        this.m_binding.nineButton.setOnClickListener { view -> processNumberButtons(view) }
+        this.m_binding.decimalButton.setOnClickListener { view -> processNumberButtons(view) }
+    }
+
+    private fun processOperatorButtons(view: View)
+    {
+
+    }
+
+    private fun processExtraButtons(view: View)
+    {
+
+    }
+
+    private fun processNumberButtons(view: View)
+    {
+        if(view.tag.toString() == ".")
+        {
+            if(!this.m_resultLabelValue.contains("."))
+            {
+                this.m_resultLabelValue += view.tag.toString()
+            }
+        }
+        else
+        {
+            this.m_resultLabelValue += view.tag.toString()
+        }
+        this.m_binding.resultTextView.text = this.m_resultLabelValue
+    }
+
+
+}
--- ICE5/app/src/main/java/ca/georgiancollege/comp3025_w2024_week5/MainActivity.kt
+++ ICE5/app/src/main/java/ca/georgiancollege/comp3025_w2024_week5/MainActivity.kt
@@ -8,8 +8,7 @@ import ca.georgiancollege.comp3025_w2024_week5.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity()
 {
     private lateinit var binding: ActivityMainBinding
-
-    private var resultLabelValue: String = ""
+    private lateinit var calculator: Calculator
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -17,60 +16,7 @@ class MainActivity : AppCompatActivity()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initializeOnClickListeners()
+        calculator = Calculator(binding)
     }
 
-    private fun initializeOnClickListeners()
-    {
-        // operator buttons
-        binding.mulitplyButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.divideButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.addButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.subtractButton.setOnClickListener { view -> processOperatorButtons(view) }
-
-        // extra buttons
-        binding.clearButton.setOnClickListener { view -> processExtraButtons(view) }
-        binding.percentButton.setOnClickListener { view -> processExtraButtons(view) }
-        binding.backspaceButton.setOnClickListener { view -> processExtraButtons(view) }
-        binding.plusMinusButton.setOnClickListener { view -> processExtraButtons(view) }
-
-        // number buttons
-        binding.zeroButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.oneButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.twoButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.threeButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.fourButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.fiveButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.sixButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.sevenButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.eightButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.nineButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.decimalButton.setOnClickListener { view -> processNumberButtons(view) }
-    }
-
-    private fun processOperatorButtons(view: View)
-    {
-
-    }
-
-    private fun processExtraButtons(view: View)
-    {
-
-    }
-
-    private fun processNumberButtons(view: View)
-    {
-        if(view.tag.toString() == ".")
-        {
-            if(!resultLabelValue.contains("."))
-            {
-                resultLabelValue += view.tag.toString()
-            }
-        }
-        else
-        {
-            resultLabelValue += view.tag.toString()
-        }
-        binding.resultTextView.text = resultLabelValue
-    }
 }
