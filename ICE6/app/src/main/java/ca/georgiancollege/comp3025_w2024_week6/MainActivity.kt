7cc89d1801ea233f47dccce87158838c825c9beb
ttsiliopoulos <ttsiliopoulos@georgiancollege.ca>
2024-02-15 15:41:58 -0500

    converted text to JSON

--- ICE6/app/src/main/java/ca/georgiancollege/comp3025_w2024_week6/MainActivity.kt
+++ ICE6/app/src/main/java/ca/georgiancollege/comp3025_w2024_week6/MainActivity.kt
@@ -3,13 +3,20 @@ package ca.georgiancollege.comp3025_w2024_week6
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println(getTextFromAsset(this, "contacts.json"))
+        for (contact in deserializeJSON()!!)
+        {
+            println(contact)
+        }
     }
 
     private fun getTextFromResource(context: Context, resourceId: Int): String
@@ -26,5 +33,15 @@ class MainActivity : AppCompatActivity() {
             .use { it.readText()}
     }
 
+    private fun deserializeJSON(): List<ContactModel>?
+    {
+        val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+        val listType = Types.newParameterizedType(List::class.java, ContactModel::class.java)
+        val adapter: JsonAdapter<List<ContactModel>> = moshi.adapter(listType)
+
+        val contactListRawString = getTextFromResource(this, R.raw.contacts)
+        val contactList: List<ContactModel>? = adapter.fromJson(contactListRawString)
+        return contactList
+    }
 
 }
