ae05c8dd0b99753ac3f09904973e56290e881d71
ttsiliopoulos <ttsiliopoulos@georgiancollege.ca>
2024-03-14 15:50:24 -0400

    added base config for Firebase

--- ICE8/app/build.gradle.kts
+++ ICE8/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -53,6 +54,10 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation ("androidx.activity:activity-ktx:1.8.2")
+
+    // firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
